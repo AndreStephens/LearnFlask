cdd7c345d88e4698a0ed54270f0a7e0e86b93227
boukman <andrestephens@outlook.com>
2023-05-21 17:13:11 -0400

    add template and static directories

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, request, render_template, redirect
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -9,9 +9,48 @@ def index():
 
 # Add page
 @app.route('/hello')
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/hello/<name>')
+def hello_world(name=None):
+    return render_template('hello.html', name=name)
 
+# Escape HTML
 @app.route('/user/<username>')
 def show_user_profile(username):
     return f"User {escape(username)}"
+
+# Enforce variable rules
+@app.route('/path/<path:subpath>') # specify "path" type
+def show_subpath(subpath):
+    return f"Subpath {escape(subpath)}"
+
+# There are different canonical conventions for using trailing slashes.
+# E.g. The projects endpoint usually has a trailing slash but not the 
+# about page
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+# To build a URL to a specific function, use the url_for() function.
+with app.test_request_context():
+    print(url_for('show_user_profile', username='John Doe'))
+    
+# By default, route only answers to GET requests, but this can be changed
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "answering to POST"
+    else:
+        return "answering to GET"
+    
+# This can also be achieved with specific methods; also see redirect function
+@app.get('/login-get')
+def login_get():
+    return redirect(url_for('login'))
+
+@app.post('/login-post')
+def login_post():
+    return redirect(url_for('login'))
